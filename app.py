ea9c9f468badbe6cbcd13fbb139fa6986e9d7bfd
ryanmuhs <ryansatria@student.telkomuniversity.ac.id>
2022-06-15 23:25:41 +0700

    editz editz...

--- app.py
+++ app.py
@@ -37,41 +37,42 @@ def inputcert():
                                         title="Choose template", 
                                         filetypes=[("PNG", "*.png"),
                                         ("All Files", "*.*")])
-        Label(root, text=template_cert, font=(font_common, 7), bg=bg_color, fg=fg_color, width=50).place(relx=.05, rely=.3)
+        entryTemplate.delete(1, END)
+        entryTemplate.insert(0, template_cert)
     except FileNotFoundError:
-        print("File not found :(")
+        mb.showerror("Error | JENERET", "File not found :(")
     else:
         return template_cert
 
 # input nama
 listName = []
-inputNama = "D:"
 def inputname():
-    # try:
-    inputNama = fd.askopenfilename(initialdir="/",
-                                title="List Nama...", 
-                                filetypes=[("Text Document", "*.txt"),
-                                ("All Files", "*.*")])
-    # Label(root, text=inputNama, font=(font_common, 7), bg=bg_color, fg=fg_color, width=100).pack(side=RIGHT)
-    Label(root, text=inputNama, font=(font_common, 7), bg=bg_color, fg=fg_color, width=50).place(relx=.05, rely=.5)
-    return inputNama
-    # except:
-        # mb.showerror("JENERET | Error","Something error")
+    try:
+        global inputNama
+        inputNama = fd.askopenfilename(initialdir="/",
+                                    title="List Nama...", 
+                                    filetypes=[("Text Document", "*.txt"),
+                                    ("All Files", "*.*")])
+        entryName.delete(1, END)
+        entryName.insert(0, inputNama)
+        print(inputNama)
+        return inputNama
+    except:
+        mb.showerror("JENERET | Error","Something error")
 
-def cleanup_data():
-    global inputNama
-    with open(inputNama) as f:
+# input nama
+def appendList():
+    inputNama = inputname()
+    with open(inputNama, "r+") as f:
         for line in f:
             listName.append(line)
             
 # generate!
-def generate_cert():
-    # cleanup_data()
+def generateCert():
     for name in enumerate(listName):
         certificate_template_image = cv2.imread()
-        cv2.putText(certificate_template_image, name.strip(), (470,514), cv2.FONT_HERSHEY_DUPLEX, 2.5, (0, 0, 0), 5, cv2.LINE_AA)
-        cv2.imwrite(f"generated-cert/{name.strip()}.png", certificate_template_image)
-        # print(f"Processing {index+1} / {len(listName)}")
+        cv2.putText(certificate_template_image, name, (470,514), cv2.FONT_HERSHEY_DUPLEX, 2.5, (0, 0, 0), 5, cv2.LINE_AA)
+        cv2.imwrite(f"generated-cert/{name}.png", certificate_template_image)
 
 # keluar
 def exit():
@@ -80,26 +81,25 @@ def exit():
         root.destroy()
 
 # template serti
-labelTemplate = Label(root, text="Insert certificate template", font=(font_title, 10), bg=bg_color, fg=fg_color)
+labelTemplate = Label(root, text="Insert certificate template", font=(font_title, 10), bg=bg_color, fg=fg_color, padx=0, pady=0)
 labelTemplate.place(relx=.05, rely=.25)
-btnList = Button(root, text="Choose file...", font=(font_title, 10), command=inputcert, width=15)
-btnList.grid(column=0, row=1)
-btnList.place(relx=.6, rely=.3)
+entryTemplate = Entry(root, text="", width=35, font=(font_common, 8))
+entryTemplate.place(relx=.05, rely=.33)
+btnTemplate = Button(root, text="Choose file...", font=(font_title, 10), command=inputcert, width=15)
+btnTemplate.grid(column=0, row=1)
+btnTemplate.place(relx=.6, rely=.3)
 
 # list nama
-labelName = Label(root, text="Insert names", font=(font_title, 10), bg=bg_color, fg=fg_color)
+labelName = Label(root, text="Insert names", font=(font_title, 10), bg=bg_color, fg=fg_color, padx=0, pady=0)
 labelName.place(relx=.05, rely=.43)
-btnNama = Button(root, text="Choose File...", font=(font_title, 10), command=inputname, width=15)
-btnNama.grid(column=0, row=1)
-btnNama.place(relx=.6, rely=.5)
-
-# directory penyimpanan fix
-# btnDir = Button(root, text="Cari direktori..", font=(font_title, 10), command=savecert, width=15)
-# btnDir.grid(column=0, row=1)
-# btnDir.place(relx=.6, rely=.5)
+entryName = Entry(root, text="", width=35, font=(font_common, 8))
+entryName.place(relx=.05, rely=.53)
+btnName = Button(root, text="Choose File...", font=(font_title, 10), command=inputname, width=15)
+btnName.grid(column=0, row=1)
+btnName.place(relx=.6, rely=.5)
 
 # generate!
-btnGenerate = Button(root, text="Generate!", font=(font_title, 10), command= lambda:cleanup_data(), bg="#029522", fg=fg_color, width=15)
+btnGenerate = Button(root, text="Generate!", font=(font_title, 10), command= generateCert, bg="#029522", fg=fg_color, width=15)
 btnGenerate.grid(column=0, row=1)
 btnGenerate.place(relx=.5, rely=.7, anchor=CENTER)
 
